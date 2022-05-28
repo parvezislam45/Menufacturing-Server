1399bb7d2b5825d12fb48b382cd353b67434355e
Parvez Islam <parvezislam45@gmail.com>
2022-05-28 17:50:09 +0600

    jwt remove

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async function run() {
 
     // ----------- All User -----------------
 
-    app.get("/user", verifyJWT, async (req, res) => {
+    app.get("/user", async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
@@ -94,7 +94,7 @@ async function run() {
       res.send({ admin: isAdmin });
     });
 
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
       const requester = req.decoded.email;
       const requesterAccount = await userCollection.findOne({
